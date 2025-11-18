00befe44d159fc4ca83dcca4610ed1a802491b8e
BitNebulaV7 <shivaq1994@gmail.com>
2025-11-18 12:09:37 +0330

    Add query: list actors per movie using INNER JOIN

--- SQL/AnalysisQueries.sql
+++ SQL/AnalysisQueries.sql
@@ -1,5 +1,4 @@
-SELECT name
-FROM sys.databases
+
 
 
 SELECT G.GenreName, COUNT(*) AS MovieCount
@@ -7,3 +6,27 @@ FROM Movies AS M
     INNER JOIN Genres AS G ON M.GenreID = G.GenreID
 GROUP BY G.GenreName
 ORDER BY MovieCount DESC
+
+
+
+-- Analysis: List all movies by genre (even non-genre films)
+-- Objective: Fully display movies and check those that don't have genre
+-- Logic: LEFT JOIN makes all movies displayed, even if their genre is NULL
+
+SELECT G.GenreName, M.Title
+FROM Movies AS M
+    LEFT JOIN Genres AS G ON M.GenreID = G.GenreID
+ORDER BY M.Title
+
+
+-- Analysis: Finding genres that don't have any films
+
+SELECT G.GenreName
+FROM Genres AS G
+    LEFT JOIN Movies AS M ON G.GenreID = M.GenreID
+
+
+SELECT G.GenreName
+FROM Genres AS G
+    LEFT JOIN Movies AS M ON G.GenreID = M.GenreID
+WHERE M.MovieID IS NULL
