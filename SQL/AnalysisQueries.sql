d18f527b9210a692468c14e9797c170e97e31060
BitNebulaV7 <shivaq1994@gmail.com>
2025-11-18 11:24:13 +0330

    Add analysis: count movies per genre

--- SQL/AnalysisQueries.sql
+++ SQL/AnalysisQueries.sql
@@ -1,2 +1,9 @@
 SELECT name
 FROM sys.databases
+
+
+SELECT G.GenreName, COUNT(*) AS MovieCount
+FROM Movies AS M
+    INNER JOIN Genres AS G ON M.GenreID = G.GenreID
+GROUP BY G.GenreName
+ORDER BY MovieCount DESC
